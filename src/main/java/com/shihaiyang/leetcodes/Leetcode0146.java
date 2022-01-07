e56491e7becee70f1eccc3910046c0ba597be29f
史海洋 <shihaiyang@shuidihuzhu.com>
2022-01-07 19:51:23 +0800

    leetcode problem 0146.LRU 算法. Map + doubly-linked list.

--- src/main/java/com/shihaiyang/leetcodes/Leetcode0146.java
+++ src/main/java/com/shihaiyang/leetcodes/Leetcode0146.java
@@ -1,5 +1,6 @@
 package com.shihaiyang.leetcodes;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -9,20 +10,21 @@ import java.util.Map;
 public class Leetcode0146 {
     public static void main(String[] args) {
         LRUCache lRUCache = new LRUCache(2);
-        lRUCache.put(1, 1); // 缓存是 {1=1}
-        lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
-        System.out.println(lRUCache.get(1));    // 返回 1
-        lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
-        System.out.println(lRUCache.get(2));    // 返回 -1 (未找到)
-        lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+        lRUCache.put(2, 1); // 缓存是 {1=1}
+        lRUCache.put(1, 1); // 缓存是 {1=1, 2=2}
+        lRUCache.put(2, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+        lRUCache.put(4, 1); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
         System.out.println(lRUCache.get(1));    // 返回 -1 (未找到)
-        System.out.println(lRUCache.get(3));    // 返回 3
-        System.out.println(lRUCache.get(4));    // 返回 4
+        System.out.println(lRUCache.get(2));    // 返回 3
     }
 }
-class LRUCache extends LinkedHashMap {
+
+/**
+ * 借助LinkedHashMap
+ */
+class LRUCache2 extends LinkedHashMap {
     private int max;
-    public LRUCache(int capacity) {
+    public LRUCache2(int capacity) {
         super(capacity, 0.75f, true);
         max = capacity;
     }
@@ -39,6 +41,93 @@ class LRUCache extends LinkedHashMap {
     protected boolean removeEldestEntry(Map.Entry eldest) {
         return super.size()>max;
     }
+}
+/**
+ * 自己写LRU的话需要怎么做？
+ * 需要一个Map，一个doubly-linked list
+ * 手动双链表吧...用LinkedHashMap太bug了.
+ */
+class LRUCache {
+    private int max;
+    private HashMap<Integer, Node> map;
+    // head最少使用，tail最新查询
+    private Node head,tail;
+    public LRUCache(int capacity) {
+        map = new HashMap<>();
+        max = capacity;
+    }
+
+    public int get(int key) {
+        Node val = map.get(key);
+        if (val == null){
+            return -1;
+        }
+        moveToLast(val);
+        return val.val;
+    }
+
+    public void put(int key, int value) {
+        if(!map.containsKey(key)){
+            Node node = addNoe(key, value);
+            map.put(key, node);
+        }else {
+            Node node = map.get(key);
+            node.val = value;
+            moveToLast(node);
+        }
+        if (removeEldestEntry()){
+            remove();
+        }
+    }
+
+    private Node addNoe(int key, int value) {
+        Node node = new Node();
+        node.key=key;
+        node.val=value;
+        if (map.size() == 0){
+            head=node;
+            tail=node;
+        }else {
+            node.pre=tail;
+            tail.next=node;
+            tail=node;
+        }
+        return node;
+    }
+
+    private void moveToLast(Node val) {
+        if(val != tail){
+            if(val == head){
+                head=head.next;
+            }
+            // move to the last
+            Node pre = val.pre, next = val.next;
+            if(pre != null){
+                pre.next=next;
+            }
+            next.pre=pre;
+            tail.next = val;
+            val.pre=tail;
+            tail = val;
+        }
+    }
+
+    private boolean removeEldestEntry() {
+        return map.size() > max;
+    }
+
+    private void remove() {
+        int key = head.key;
+        head = head.next;
+        head.pre=null;
+        map.remove(key);
+    }
+}
+class Node{
+    int key;
+    int val;
+    Node pre;
+    Node next;
 }
 
 /**

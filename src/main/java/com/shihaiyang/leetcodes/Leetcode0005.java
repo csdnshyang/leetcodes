c9a23c9ff9b0444dbea8a5b888c8fc8a9972c270
史海洋 <csdnshyang@gmail.com>
2022-01-10 00:52:14 +0800

    leetcode problem 0005. 最长回文子串.[双指针].

---
+++ src/main/java/com/shihaiyang/leetcodes/Leetcode0005.java
@@ -0,0 +1,12 @@
+// 05. 最长回文子串.[双指针].
+
+class Solution {
+    public String longestPalindrome(String s) {
+      int index;
+      int maxLen=0,start=0,end=0;
+      for(index=0;index<s.lenght();index++){
+        int middle = index+s.lenght();
+        
+      }
+    }
+}

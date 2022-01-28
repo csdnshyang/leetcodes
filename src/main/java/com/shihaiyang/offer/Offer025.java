667d15a98493c875ecf4a4f50ca63217e4176c39
史海洋 <shihaiyang@shuidihuzhu.com>
2022-01-29 00:17:06 +0800

    Offer025. 链表中的两数相加.[栈，返回新链表5ms].

--- src/main/java/com/shihaiyang/offer/Offer025.java
+++ src/main/java/com/shihaiyang/offer/Offer025.java
@@ -5,7 +5,7 @@ import com.shihaiyang.leetcodes.ListNode;
 import java.util.Stack;
 
 // 剑指 Offer II 025. 链表中的两数相加
-// Offer025. 链表中的两数相加.[栈，返回新链表].
+// Offer025. 链表中的两数相加.[栈，返回新链表5ms].
 public class Offer025 {
 }
 
@@ -25,6 +25,35 @@ class SolutionOffer025 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         Stack<Integer> stack1 = new Stack<>();
         Stack<Integer> stack2 = new Stack<>();
-        return null;
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        ListNode dummy = new ListNode();
+        int flag = 0;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || flag == 1) {
+            int val = flag;
+            if (!stack1.isEmpty()) {
+                val += stack1.pop();
+            }
+            if (!stack2.isEmpty()) {
+                val += stack2.pop();
+            }
+            if (val >= 10) {
+                val = val % 10;
+                flag = 1;
+            } else {
+                flag = 0;
+            }
+            ListNode tmp = new ListNode();
+            tmp.val = val;
+            tmp.next = dummy.next;
+            dummy.next = tmp;
+        }
+        return dummy.next;
     }
 }

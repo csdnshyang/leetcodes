848c27ad4ce8a4f6ba13103b157c227b564aa0f7
史海洋 <shihaiyang@shuidihuzhu.com>
2022-02-07 19:08:31 +0800

    1219. 黄金矿工.[回溯 17ms].

--- src/main/java/com/shihaiyang/daily/Leetcode1219.java
+++ src/main/java/com/shihaiyang/daily/Leetcode1219.java
@@ -2,7 +2,18 @@ package com.shihaiyang.daily;
 
 // 1219. 黄金矿工.[回溯 2ms].
 public class Leetcode1219 {
-
+    public static void main(String[] args) {
+        Solution1219 solution1219 = new Solution1219();
+        int grid[][] = new int[][]{
+                {1,0,7},
+                {2,0,6},
+                {3,4,5},
+                {0,3,0},
+                {9,0,20},
+        };
+        int maximumGold = solution1219.getMaximumGold(grid);
+        System.out.println(maximumGold);
+    }
 }
 
 /**
@@ -33,44 +44,49 @@ public class Leetcode1219 {
  */
 
 /**
- * BFS + 回溯 + 多源
+ * DFS + 回溯 + 多源
  * 走过就标记为0
+ * 多源暴力DFS + 回溯
+ * 多源dfs就是每个非0点都尝试一下，看能获取最大的是多少。
+ * 回溯是指，每个访问过的点都设置为0.当遍历结束再设置回去，这样省去了每次都copy一个二维数组的空间。
+ * 深度遍历DFS不需要额外的空间，上下左右扫即可。
+ * 广度遍历BFS需要队列，一层一层扫。
+ *
+ * 1. 遍历每一个节点，对每个节点进行深度遍历。
+ * 2. 遍历过程：比较获取的gold。
+ *      然后改为0，
+ *      再进行深度遍历
+ *      遍历结束之后再赋值回原值。
+ * 3. 返回最大的gold
  */
 class Solution1219 {
-    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    int[][] grid;
-    int m, n;
+    static int dirs[][] = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     int ans = 0;
-
     public int getMaximumGold(int[][] grid) {
-        this.grid = grid;
-        this.m = grid.length;
-        this.n = grid[0].length;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] != 0) {
-                    dfs(i, j, 0);
+                    dfs(grid, i, j, 0);
                 }
             }
         }
         return ans;
     }
 
-    public void dfs(int x, int y, int gold) {
-        gold += grid[x][y];
+    public void dfs(int[][] grid, int x1, int y1, int gold){
+        int rec = grid[x1][y1];
+        gold += rec;
         ans = Math.max(ans, gold);
-
-        int rec = grid[x][y];
-        grid[x][y] = 0;
-
-        for (int d = 0; d < 4; ++d) {
-            int nx = x + dirs[d][0];
-            int ny = y + dirs[d][1];
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n && grid[nx][ny] > 0) {
-                dfs(nx, ny, gold);
+        // 设置为0，标识已经扫描过
+        grid[x1][y1] = 0;
+        for (int i = 0; i < dirs.length; i++) {
+            int x = x1 + dirs[i][0];
+            int y = y1 + dirs[i][1];
+            if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] != 0) {
+                dfs(grid, x, y, gold);
             }
         }
-
-        grid[x][y] = rec;
+        // 回溯：设置会原值
+        grid[x1][y1] = rec;
     }
 }

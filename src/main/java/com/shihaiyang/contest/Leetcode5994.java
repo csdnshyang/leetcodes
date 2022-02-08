55033ef19f54a974033937f360472349e90b3227
史海洋 <shihaiyang@shuidihuzhu.com>
2022-02-08 15:44:54 +0800

    5994. 查找给定哈希值的子串.[反向滑动窗口,取模公式消除大数10ms].

--- src/main/java/com/shihaiyang/contest/Leetcode5994.java
+++ src/main/java/com/shihaiyang/contest/Leetcode5994.java
@@ -1,22 +1,20 @@
 package com.shihaiyang.contest;
 
-import java.math.BigDecimal;
-
-// 5994. 查找给定哈希值的子串
+// 5994. 查找给定哈希值的子串.[反向滑动窗口,取模公式消除大数10ms].
 
 /**
- *  * "xmmhdakfursinye"
- *  * 96
- *  * 45
- *  * 15
- *  * 21
+ * * "xmmhdakfursinye"
+ * * 96
+ * * 45
+ * * 15
+ * * 21
  */
 public class Leetcode5994 {
     public static void main(String[] args) {
         Solution5994 solution5994 = new Solution5994();
 //        String leetcode = solution5994.subStrHash("fbxzaad", 31, 100, 3, 32);
 //        String leetcode = solution5994.subStrHash("leetcode", 7, 20, 2, 0);
-        String leetcode = solution5994.subStrHash("xqgfatvtlwnnkxipmipcpqwbxihxblaplpfckvxtihonijhtezdnkjmmk",22, 51,41,9);
+        String leetcode = solution5994.subStrHash("xqgfatvtlwnnkxipmipcpqwbxihxblaplpfckvxtihonijhtezdnkjmmk", 22, 51, 41, 9);
         System.out.println(leetcode);
     }
 }
@@ -39,8 +37,9 @@ public class Leetcode5994 {
  * "bxz" 的哈希值为 hash("bxz", 31, 100) = (2 * 1 + 24 * 31 + 26 * 312) mod 100 = 25732 mod 100 = 32 。
  * "fbx" 是长度为 3 的第一个哈希值为 32 的子串，所以我们返回 "fbx" 。
  * 注意，"bxz" 的哈希值也为 32 ，但是它在字符串中比 "fbx" 更晚出现。
- *
+ * <p>
  * "xqgfatvtlwnnkxipmipcpqwbxihxblaplpfckvxtihonijhtezdnkjmmk",22, 51,41,9
+ * "xqgfatvtlwnnkxipmipcpqwbxihxblaplpfckvxti"
  * "xqgfatvtlwnnkxipmipcpqwbxihxblaplpfckvxti"
  */
 
@@ -54,29 +53,43 @@ public class Leetcode5994 {
  * (a+b) % p = (a%p + b%p) % p
  * (a^b) % p = ((a%p)^b) % p
  * (a-b) % p = (a%p - b%p) % p
+ * 防止数字越界，所以要利用乘法计算出power^k%p的值。提公因式power。
+ * (a * power^1 + b * power^2) % p
+ * = (b*power*powder + a*power ) %p
+ * = ((b*power+a) * power)  %p
+ * = ((b*power+a) %p * power) %p
+ * 这样切分计算，就不会出现大数。
  */
 class Solution5994 {
     public String subStrHash(String s, int power, int modulo, int k, int hashValue) {
-        int index = -1;
-        for (int i = s.length() - k - 1; i >= 0; i--) {
-            long hashSub = 0;
-            for (int j = 0; j < k; j++) {
-                hashSub += hashChar(s.charAt(i + j), j, power, modulo);
-                hashSub %= modulo;
-            }
-            if (hashSub == hashValue) {
-                index = i;
-            }
+        // 计算第一个子串  size=5  k=2   第一个子串[4,3]
+        char[] arr = s.toCharArray();
+        int ans = 0, n = s.length();
+        long hash = 0, mod = 1;
+        // k-1个初始化
+        for (int i = n - 1; i >= n - k + 1; i--) {
+            mod = mod * power % modulo;
+            hash = power * (hash + index(arr[i])) % modulo;
         }
-        return index == -1 ? "" : s.substring(index, k + index);
+        // 一次计算前面的子串
+        for (int i = s.length() - k; i >= 0; i--) {
+            // k-1个加上头的1个，凑k个  第一个是p^0=1 即charIndex本身
+            hash = (index(arr[i]) + hash) % modulo;
+            if (hash == hashValue) {
+                ans = i;
+            }
+            // 去除最后一个，给下一个子串用  最后一个(charIndex*power^(k-1)) % p = (charIndex * (power^(k-1) % p)) % p
+            // ∵ power^(k-1)%p = mod
+            // ∴ (charIndex * (power^(k-1) % p)) % p = (charIndex*mod) %p
+            long lastHash = (index(arr[i + k - 1]) * mod) % modulo;
+            // 把去除最后一个字符的hash*power当做下一个子串的后k-1个字符的hash值
+            // hash - lastHash 就行，但是防止hash更小出现负数，所以+modulo。
+            hash = power * (hash - lastHash + modulo) % modulo;
+        }
+        return s.substring(ans, k + ans);
     }
 
-    // val(s[0]) * p0
-    public long hashChar(char c, int k, int power, int modulo) {
-        long index = c - 'a' + 1;
-        power %= modulo;
-        long pow = (long)Math.pow(power, k);
-        pow %= modulo;
-        return (index * pow) % modulo;
+    public int index(char c) {
+        return c - 'a' + 1;
     }
 }

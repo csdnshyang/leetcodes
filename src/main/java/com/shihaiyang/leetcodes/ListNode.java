ef63ff655e8020597c7f1c9a48772c89bf221842
史海洋 <shihaiyang@shuidihuzhu.com>
2022-02-09 17:28:24 +0800

    根据数组初始化链表公共方法

--- src/main/java/com/shihaiyang/leetcodes/ListNode.java
+++ src/main/java/com/shihaiyang/leetcodes/ListNode.java
@@ -6,4 +6,16 @@ public class ListNode {
     public ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public static ListNode generate(int[] arr) {
+        ListNode dummy = new ListNode();
+        ListNode p = dummy;
+        for (int i = 0; i < arr.length; i++) {
+            ListNode temp = new ListNode();
+            temp.val = arr[i];
+            p.next =temp;
+            p = temp;
+        }
+        return dummy.next;
+    }
  }

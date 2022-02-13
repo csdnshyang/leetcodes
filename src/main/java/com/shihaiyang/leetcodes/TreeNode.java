29192b20376b97c9565bd07c449c4bd87e8c5180
史海洋 <csdnshyang@gmail.com>
2022-02-14 00:37:59 +0800

    初始化二叉树通用方法

--- src/main/java/com/shihaiyang/leetcodes/TreeNode.java
+++ src/main/java/com/shihaiyang/leetcodes/TreeNode.java
@@ -17,4 +17,26 @@ public class TreeNode {
         this.left = left;
         this.right = right;
     }
+
+    // [1,3,2,5,3,null,9]
+    public static TreeNode generate(Integer[] vals) {
+        if (vals.length == 0) {
+            return new TreeNode();
+        }
+        TreeNode[] arr = new TreeNode[vals.length];
+        for (int i = 0; i < vals.length; i++) {
+            if (vals[i] != null) {
+                TreeNode node = new TreeNode(vals[i]);
+                arr[i] = node;
+                int parent = (i-1) / 2;
+                boolean leftFlag = ((i & 1) == 1);
+                if (leftFlag) {
+                    arr[parent].left = node;
+                } else {
+                    arr[parent].right = node;
+                }
+            }
+        }
+        return arr[0];
+    }
 }

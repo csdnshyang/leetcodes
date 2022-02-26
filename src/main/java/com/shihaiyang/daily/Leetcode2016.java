66aed736c2550829b37dc91075fc62f3a5c6123e
史海洋 <csdnshyang@gmail.com>
2022-02-26 12:11:26 +0800

    2016. 增量元素之间的最大差值[前缀最小值+实时最大差值 0ms].

--- src/main/java/com/shihaiyang/daily/Leetcode2016.java
+++ src/main/java/com/shihaiyang/daily/Leetcode2016.java
@@ -38,22 +38,23 @@ public class Leetcode2016 {
         Assertions.assertEquals(solution0033.maximumDifference(new int[]{1, 5, 2, 10}), 9);
         Assertions.assertEquals(solution0033.maximumDifference(new int[]{7,1,5,4}), 4);
         Assertions.assertEquals(solution0033.maximumDifference(new int[]{9,4,3,2}), -1);
+        Assertions.assertEquals(solution0033.maximumDifference(new int[]{999,997,980,976,948,940,938,928,924,917,907,907,881,878,864,862,859,857,848,840,824,824,824,805,802,798,788,777,775,766,755,748,735,732,727,705,700,697,693,679,676,644,634,624,599,596,588,583,562,558,553,539,537,536,509,491,485,483,454,449,438,425,403,368,345,327,287,285,270,263,255,248,235,234,224,221,201,189,187,183,179,168,155,153,150,144,107,102,102,87,80,57,55,49,48,45,26,26,23,15}), -1);
     }
 }
 /*
 前缀最小值 以及比较最大
-如果有更小值。就更新前缀最小值。
-如果比最小值大，计算差值，比较之前差值。
+如果当前值严格大于前缀最小，就计算一下
+否则更新最小前缀。
 1 <= nums[i] <= 109
  */
 class Solution2016 {
     public int maximumDifference(int[] nums) {
         int prev = nums[0], ans = -1;
         for (int i = 1; i < nums.length; i++) {
-            if (prev >= nums[i]) {
-                prev = nums[i];
-            } else {
+            if (nums[i] > prev) {
                 ans = Math.max(ans, nums[i] - prev);
+            } else {
+                prev = nums[i];
             }
         }
         return ans;

20ecbb726c8b2ea91cc23c9a5591c11f52f358ad
史海洋 <shihaiyang@shuidihuzhu.com>
2022-03-17 16:41:54 +0800

    0432. 全 O(1) 的数据结构.[双向链表].

--- src/main/java/com/shihaiyang/daily/Leetcode0432.java
+++ src/main/java/com/shihaiyang/daily/Leetcode0432.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 // 0432. 全 O(1) 的数据结构
 /*
@@ -44,78 +46,167 @@ public class Leetcode0432 {
         allOne.inc("leet");
         Assertions.assertEquals(allOne.getMaxKey(), "hello"); // 返回 "hello"
         Assertions.assertEquals(allOne.getMinKey(), "leet"); // 返回 "leet"
+        allOne.inc("hello");
+        allOne.dec("leet");
+        Assertions.assertEquals(allOne.getMaxKey(), "hello"); // 返回 "hello"
+        Assertions.assertEquals(allOne.getMinKey(), "hello"); // 返回 "hello"
+        allOne.dec("leet");
+        allOne.inc("leet");
+        allOne.inc("leet");
+        allOne.inc("leet2");
+        allOne.inc("leet3");
+        Assertions.assertEquals(allOne.getMaxKey(), "hello"); // 返回 "hello"
+        Assertions.assertEquals(allOne.getMinKey(), "leet3"); // 返回 "hello"
     }
+
+    //["AllOne","inc","inc","inc","inc","getMaxKey","inc","inc","inc","dec","inc","inc","inc","getMaxKey"]
+    //[[],["hello"],["goodbye"],["hello"],["hello"],[],["leet"],["code"],["leet"],["hello"],["leet"],["code"],["code"],[]]
+    @Test
+    public void testcase2() {
+        AllOne allOne = new AllOne();
+        allOne.inc("hello");
+        allOne.inc("goodbye");
+        allOne.inc("hello");
+        allOne.inc("hello");
+        allOne.inc("leet");
+        allOne.inc("code");
+        allOne.inc("leet");
+        allOne.dec("hello");
+        allOne.inc("leet");
+        allOne.inc("code");
+        allOne.inc("code");
+        Assertions.assertTrue(allOne.getMaxKey().equals("leet") || allOne.getMaxKey().equals("code"));
+    }
+
 }
+
 /*
 Map+doubly-linked
 Map存储字符串对应的Node节点。
-Node是带有头尾指针的双向链表。当遇到最大，或者最小时，移动节点。
-如果cnt=0。删除节点。
-如果cnt<min.
-如果cnt>max.
-字符串加
-1. 如果节点存在
-    如果超过左侧节点。交换节点顺序。
-    如果左侧节点是head节点。head节点指向当前节点。
-2. 如果节点不存在
-    添加到有次。更新tail节点
-字符串减
-1. 如果节点存在
-    
+Node是带哨兵的双向链表。
+从小打大的顺序。
+inc的时候
+如果不存在，判断是next是哨兵或者next>1,就在root后加节点。
+如果存在，如果是next是哨兵，或者next > +1，就在cur后面加节点。
+dec的时候
+如果cur cnt=1;直接从node key中移除
+如果cur pre==root或者pre<cnt-1   ,pre后加节点
+否则pre key加key元素
+如果node key是空，node移除
+
  */
 class AllOne {
-    DNode head;
-    DNode tail;
+    DNode root; // 哨兵
     Map<String, DNode> map;
+
     public AllOne() {
         map = new HashMap<>();
-
+        // 带头尾结点
+        root = new DNode("", 0);
+        root.next = root;
+        root.pre = root;
     }
 
     public void inc(String key) {
+        /*
+        新增：
+        如果存在key
+            curNode.pre 的cnt是否等于inc。
+                如果相等，直接加入key
+                如果不相等，说明pre的cnt更大，需要在pre后加一个inc节点
+            key从curNode中移除;如果key为空，移除curNode;
+         */
         if (map.containsKey(key)) {
-            DNode dNode = map.get(key);
-            int inc = dNode.inc();
-            // 修改的节点左移一下。
-            if (dNode.pre.cnt < inc) {
-
+            DNode curNode = map.get(key);
+            DNode next = curNode.next;
+            if (next == root || next.cnt > curNode.cnt + 1) {
+                map.put(key, curNode.insert(new DNode(key, curNode.cnt + 1)));
+            } else {
+                next.keys.add(key);
+                map.put(key, next);
+            }
+            // 从key从当前node移除，如果key为空，就移除node
+            curNode.keys.remove(key);
+            if (curNode.keys.isEmpty()) {
+                curNode.remove();
             }
         } else {
-            DNode dNode = new DNode(key);
-            map.put(key, dNode);
+            /*
+            如果不存在
+                root.next==root,说明没有节点。在head后加新节点
+                tail.pre.cnt == 1,说明有只出现一次的节点,tail.pre中加入key
+                tail.pre.cnt > 1,说明没有只出现一次的节点,tail.pre后加入新节点
+             */
+            if (root.next == root || root.next.cnt > 1) {
+                map.put(key, root.insert(new DNode(key, 1)));
+            } else {
+                root.next.keys.add(key);
+                map.put(key, root.next);
+            }
         }
     }
 
+    /*
+    cnt-1情况
+    如果存在key
+        dec=curNode.cnt-1;
+        curNode.next.cnt == dec; 在next.key中加入key
+        !=dec curNode后面加入新node
+        curNode中移除key;如果curNode.key.isEmpty();移除curNode
+     */
     public void dec(String key) {
-
+        if (map.containsKey(key)) {
+            DNode curNode = map.get(key);
+            if (curNode.cnt == 1) {
+                map.remove(key);
+            }else{
+                DNode pre = curNode.pre;
+                if (pre == root || pre.cnt < curNode.cnt - 1) {
+                    map.put(key, pre.insert(new DNode(key, curNode.cnt - 1)));
+                } else {
+                    pre.keys.add(key);
+                    map.put(key, pre);
+                }
+            }
+            curNode.keys.remove(key);
+            if (curNode.keys.isEmpty()) {
+                curNode.remove();
+            }
+        }
     }
 
     public String getMaxKey() {
-        return head.key;
+        return root.pre != null ? root.pre.keys.iterator().next() : "";
     }
 
     public String getMinKey() {
-        return tail.key;
+        return root.next != null ? root.next.keys.iterator().next() : "";
     }
 }
-class DNode{
-    public String key;
+
+class DNode {
+    public Set<String> keys;
     public int cnt = 0;
     public DNode pre;
     public DNode next;
 
-    public DNode(String key) {
-        this.key = key;
-        this.cnt = 1;
+    public DNode(String key, int cnt) {
+        this.keys = new HashSet<>();
+        this.keys.add(key);
+        this.cnt = cnt;
     }
 
-    public int inc() {
-        cnt++;
-        return cnt;
+    // 在this后加入一个node节点
+    public DNode insert(DNode dNode) {
+        dNode.pre = this;
+        dNode.next = this.next;
+        dNode.next.pre = dNode;
+        dNode.pre.next = dNode;
+        return dNode;
     }
 
-    public int dec() {
-        cnt--;
-        return cnt;
+    public void remove() {
+        this.pre.next = this.next;
+        this.next.pre = this.pre;
     }
 }

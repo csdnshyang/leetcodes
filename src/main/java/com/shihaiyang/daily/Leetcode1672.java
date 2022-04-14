72982d6171c65cd2f5148eb73f71b74c55703f3c
史海洋 <shihaiyang@shuidihuzhu.com>
2022-04-14 10:11:25 +0800

    1672. 最富有客户的资产总量.[双层遍历0ms].

---
+++ src/main/java/com/shihaiyang/daily/Leetcode1672.java
@@ -0,0 +1,63 @@
+package com.shihaiyang.daily;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+// 1672. 最富有客户的资产总量.[双层遍历0ms].
+/*
+给你一个 m x n 的整数网格 accounts ，其中 accounts[i][j] 是第 i​​​​​​​​​​​​ 位客户在第 j 家银行托管的资产数量。返回最富有客户所拥有的 资产总量 。
+
+客户的 资产总量 就是他们在各家银行托管的资产数量之和。最富有客户就是 资产总量 最大的客户。
+
+ 
+
+示例 1：
+
+输入：accounts = [[1,2,3],[3,2,1]]
+输出：6
+解释：
+第 1 位客户的资产总量 = 1 + 2 + 3 = 6
+第 2 位客户的资产总量 = 3 + 2 + 1 = 6
+两位客户都是最富有的，资产总量都是 6 ，所以返回 6 。
+示例 2：
+
+输入：accounts = [[1,5],[7,3],[3,5]]
+输出：10
+解释：
+第 1 位客户的资产总量 = 6
+第 2 位客户的资产总量 = 10
+第 3 位客户的资产总量 = 8
+第 2 位客户是最富有的，资产总量是 10
+示例 3：
+
+输入：accounts = [[2,8,7],[7,1,3],[1,9,5]]
+输出：17
+ */
+public class Leetcode1672 {
+    Solution1672 solution1672 = new Solution1672();
+
+    @Test
+    public void testcase1() {
+        int maximumWealth = solution1672.maximumWealth(new int[][]{
+                {1, 2, 3}, {3, 2, 1}
+        });
+        Assertions.assertEquals(maximumWealth, 6);
+    }
+
+}
+/*
+
+ */
+class Solution1672 {
+    public int maximumWealth(int[][] accounts) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
+            }
+            max = Math.max(sum, max);
+        }
+        return max;
+    }
+}
